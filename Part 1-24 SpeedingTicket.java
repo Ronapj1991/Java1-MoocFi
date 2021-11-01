03b6a1d014ea35ff569b304c081f20fd5ae8aaf9
Ronapj1991 <51872051+Ronapj1991@users.noreply.github.com>
2021-11-01 12:35:40 -0400

    Create Part 1-24 SpeedingTicket.java

---
+++ Part 1-24 SpeedingTicket.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+ 
+
+public class SpeedingTicket {
+
+ 
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+ 
+
+        // Write your program here. 
+
+        System.out.println("Give speed:");
+
+        int speedNum = Integer.valueOf(scanner.nextLine());
+
+ 
+
+        if (speedNum >  120)
+
+            System.out.println("Speeding ticket!");
+
+    }
+
+}
